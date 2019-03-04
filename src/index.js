8051df9514f6d57b4ddc6a43d71fa60ffd2f0aaa
Vladislav Lisetskii <vladislav_lisetskii@epam.com>
2019-03-04 12:18:02 +0300

    feat: implement loveTriangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    const loverA = preferences[i];
+    const loverB = preferences[loverA - 1];
+    const loverC = preferences[loverB - 1];
+    if ((i + 1) == loverC && (i + 1) != loverA) {
+      count++;
+      preferences[i] = 0;
+      preferences[loverA - 1] = 0;
+      preferences[loverB - 1] = 0;
+    }
+  }
+  return count;
 };
